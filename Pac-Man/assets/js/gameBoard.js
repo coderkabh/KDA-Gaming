8876a4131ea8ac62cf8adc084e4f1e6cc2f3594b
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-12-04 22:23:50 +0530

    ghost moves

--- Pac-Man/assets/js/gameBoard.js
+++ Pac-Man/assets/js/gameBoard.js
@@ -1,56 +1,81 @@
-import { GRID_SIZE , CELL_SIZE , OBJECT_TYPE , CLASS_LIST } from '../Pac-Man-setup';
+import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from './setup';
 
-class GameBoard{
-    constructor(DOMGrid){
-        this.dotCount = 0;
-        this.grid = [];
-        this.DOMGrid = DOMGrid ;
-    }
+class GameBoard {
+  constructor(DOMGrid) {
+    this.dotCount = 0;
+    this.grid = [];
+    this.DOMGrid = DOMGrid;
+  }
 
-    showGameStatus(gameWin){ // tell us if we won the game or over
-        const div = document.createElement('div');
-        div.classList.add('game-status');
-        div.innerHTML = `${gameWin ? 'WIN!!' : 'GAME OVER!!'}`;
-        this.DOMGrid.appendChild(div);
-    }
+  showGameStatus(gameWin) {
+    // Create and show game win or game over
+    const div = document.createElement('div');
+    div.classList.add('game-status');
+    div.innerHTML = `${gameWin ? 'WIN!' : 'GAME OVER!'}`;
+    this.DOMGrid.appendChild(div);
+  }
 
-    createGrid(level){
-        this.dotCount =0;
-        this.grid = [];
-        this.DOMGrid.innerHTML = '';
-        this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE} , ${CELL_SIZE}px)`;
+  createGrid(level) {
+    this.dotCount = 0;
+    this.grid = [];
+    this.DOMGrid.innerHTML = '';
+    // First set correct amount of columns based on Grid Size and Cell Size
+    this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px);`;
 
-        level.forEach(square => {
-            const div  = document.createElement('div');
-           div.classList.add('square' , CLASS_LIST[square]) ;
-           div.style.cssText = `width: ${CELL_SIZE}px ; height:${CELL_SIZE}px`;
-            this.DOMGrid.appendChild(div);
-            this.grid.push(div);
+    level.forEach((square) => {
+      const div = document.createElement('div');
+      div.classList.add('square', CLASS_LIST[square]);
+      div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
+      this.DOMGrid.appendChild(div);
+      this.grid.push(div);
 
-            if(CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
-            
-        });
-    }
-    addobject(pos,classes){
-        this.grid[pos].classList.add(...classes);
-    }
-    removeobject(pos,classes){
-        this.grid[pos].classList.remove(...classes);
-    }
+      // Add dots
+      if (CLASS_LIST[square] === OBJECT_TYPE.DOT) this.dotCount++;
+    });
+  }
 
-    objectExist(pos,object){
-        return this.grid[pos].classList.contains(object);
-    }
+  addObject(pos, classes) {
+    this.grid[pos].classList.add(...classes);
+  }
 
-    rotateDiv(pos,deg){
-        this.grid[pos].style.transform = `rotate(${deg}deg)`;
-    }
+  removeObject(pos, classes) {
+    this.grid[pos].classList.remove(...classes);
+  }
+  // Can have an arrow function here cause of this binding
+  objectExist(pos, object) {
+    return this.grid[pos].classList.contains(object);
+  };
 
-    static createGameBoard(DOMGrid,level){
-        const board = new this(DOMGrid);
-        board.createGrid(level);
-        return board;
+  rotateDiv(pos, deg) {
+    this.grid[pos].style.transform = `rotate(${deg}deg)`;
+  }
+
+  moveCharacter(character) {
+    if (character.shouldMove()) {
+      const { nextMovePos, direction } = character.getNextMove(
+        this.objectExist.bind(this)
+      );
+      const { classesToRemove, classesToAdd } = character.makeMove();
+
+      if (character.rotation && nextMovePos !== character.pos) {
+        // Rotate
+        this.rotateDiv(nextMovePos, character.dir.rotation);
+        // Rotate the previous div back
+        this.rotateDiv(character.pos, 0);
+      }
+
+      this.removeObject(character.pos, classesToRemove);
+      this.addObject(nextMovePos, classesToAdd);
+
+      character.setNewPos(nextMovePos, direction);
     }
+  }
+
+  static createGameBoard(DOMGrid, level) {
+    const board = new this(DOMGrid);
+    board.createGrid(level);
+    return board;
+  }
 }
 
 export default GameBoard;
